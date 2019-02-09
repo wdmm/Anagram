e503a1788b0702245e7d09fad8f159d5822cbadf
Wdm M <13vadimm@gmail.com>
2019-02-09 16:35:33 +0200

    Update Inther.java

--- Inther.java
+++ Inther.java
@@ -3,13 +3,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-class Inther
-{
+class Inther {
 	// Metoda ce verifica daca 2 string'uri sunt anagrame
-	public static boolean esteAnagrama(String str1, String str2)
-	{
-		if ( str1.length() != str2.length() ) 
-		{
+	public static boolean esteAnagrama(String str1, String str2) {
+		if ( str1.length() != str2.length() ) {
             		return false;
         	}
 
@@ -29,8 +26,7 @@ class Inther
 	}
 	
 	// Metoda main
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		// Variabila ce va contine toate cuvintele citite din fisier
 		ArrayList<String> docStr = new ArrayList<String>();
 		/* Variabila currentS va contine toate cuvintele ce au anagrame
@@ -38,16 +34,13 @@ class Inther
 		 */
 		String currentS = "", alternateS = "";
 
-		if (args.length == 1 )
-		{
+		if (args.length == 1 ) {
 			String FILENAME = args[0];
-			try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) 
-			{
+			try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
 				String sCurrentLine;
 				while ((sCurrentLine = br.readLine()) != null) docStr.add(sCurrentLine);
 			} 
-			catch (IOException e) 
-			{
+			catch (IOException e) {
 				System.err.println("\nSpecificati corect calea catre fisier, " +
 						"este primita ca primul argument.");
 				System.exit(1);
@@ -55,26 +48,20 @@ class Inther
 		}
 		
 
-		for (int i = 0; i < docStr.size(); i++)
-		{
-			for (int j = i+1; j < docStr.size(); j++)
-			{
-				if (esteAnagrama(docStr.get(i),docStr.get(j)))
-				{
-					if (!currentS.contains(docStr.get(i)))
-					{
+		for (int i = 0; i < docStr.size(); i++) {
+			for (int j = i+1; j < docStr.size(); j++) {
+				if (esteAnagrama(docStr.get(i),docStr.get(j))) {
+					if (!currentS.contains(docStr.get(i))) {
 						currentS += docStr.get(i) + " ";
 						alternateS += docStr.get(i) + " ";
 					} 
-					if (!currentS.contains(docStr.get(j)))
-					{
+					if (!currentS.contains(docStr.get(j))) {
 						currentS += docStr.get(j) + " ";
 						alternateS += docStr.get(j) + " ";
 					}
 				}
 			}
-			if (!alternateS.isEmpty())
-			{
+			if (!alternateS.isEmpty()) {
 				System.out.println(alternateS);
 				alternateS = "";
 			}
